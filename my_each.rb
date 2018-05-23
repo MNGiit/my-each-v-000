54c63e127f7d629b03b3f34f3b0d0ff83eba9b31
Mohammed Noori <kmanoorib@gmail.com>
2018-05-23 21:22:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,7 @@
 def my_each (array)# put argument(s) here
   # code here
-  i = 0
-  while i < array.length
-    array.collection do |number|
-      puts number
-    end
-    i+=1
-  end
+  array.collect do |i|
+    
+  
   array
 end
